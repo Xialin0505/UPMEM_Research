b549fbc74d8e6b89bb2c96ada5278f21ee7363bc
Xialin Liu <xialinl@upmemcloud3.cloud.upmem.com>
2024-03-09 21:46:42 +0100

    clean up

--- libaray/src/backends/build-dir/verbose/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/upmem0037/xialinl/research/libaray/src/backends/verbose
-# Build directory: /home/upmem0037/xialinl/research/libaray/src/backends/build-dir/verbose
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- libaray/src/backends/build-dir/vpd/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/upmem0037/xialinl/research/libaray/src/backends/vpd
-# Build directory: /home/upmem0037/xialinl/research/libaray/src/backends/build-dir/vpd
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
