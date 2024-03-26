808f10259df38062c780a17c6e0ff53b39e73848
Xialin Liu <xialinl@upmemcloud5.cloud.upmem.com>
2024-03-26 22:18:05 +0100

    fix linking

--- dpu_demo/checksum/link.sh
+++ dpu_demo/checksum/link.sh
@@ -2,8 +2,10 @@
 
 cp ~/research/libaray/src/backends/build-dir/api/libdpu.so.0.0 ./lib 
 cp ~/research/libaray/src/backends/build-dir/api/libdpujni.so.0.0 ./lib
+cp ~/research/libaray/src/backends/build-dir/api/libdpu.so ./lib
 cp /lib/libdpufsim.so ./lib/
-mv ./lib/libdpu.so.0.0 ./lib/libdpu.so.2023.2
-mv ./lib/libdpujni.so.0.0 ./lib/libdpujni.so.2023.2
+# cp /lib/libdpuverbose.so.2023.2 ./lib/
+# mv ./lib/libdpu.so.0.0 ./lib/libdpu.so.2023.2
+# mv ./lib/libdpujni.so.0.0 ./lib/libdpujni.so.2023.2
 
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH;$PWD/lib"
