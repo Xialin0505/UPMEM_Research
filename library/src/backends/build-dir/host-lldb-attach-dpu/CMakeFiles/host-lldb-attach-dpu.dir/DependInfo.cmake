5dba5fad5c2e5fb3263c51919ac538b29c3afa5d
Xialin Liu <xialinl@upmemcloud3.cloud.upmem.com>
2024-04-06 20:10:24 +0200

    cleanup

---
+++ library/src/backends/build-dir/host-lldb-attach-dpu/CMakeFiles/host-lldb-attach-dpu.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/upmem0037/xialinl/research/libaray/src/backends/host-lldb-attach-dpu/lldb-attach-dpu.c" "/home/upmem0037/xialinl/research/libaray/src/backends/build-dir/host-lldb-attach-dpu/CMakeFiles/host-lldb-attach-dpu.dir/lldb-attach-dpu.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DPU_TOOLS_VERSION=0.0"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../host-lldb-attach-dpu/../api/include/lowlevel"
+  "../host-lldb-attach-dpu/../api/include/api"
+  "../api/include/lowlevel"
+  "../api/include/api"
+  "../api/src/include"
+  "../api/src/api/include"
+  "../api/../ufi/src"
+  "../api/../ufi/include"
+  "../api/../commons/include"
+  "../api/../commons/src"
+  "../api/../commons/src/properties"
+  "../api/../verbose/src"
+  "/usr/include/python3.7m"
+  "gen/include"
+  "../verbose/src"
+  "../verbose/../commons/include"
+  "../verbose/../api/include/api"
+  "../verbose/../api/include/lowlevel"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/upmem0037/xialinl/research/libaray/src/backends/build-dir/api/CMakeFiles/dpu.dir/DependInfo.cmake"
+  "/home/upmem0037/xialinl/research/libaray/src/backends/build-dir/verbose/CMakeFiles/dpuverbose.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ library/src/backends/build-dir/hw/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/upmem0037/xialinl/research/libaray/src/backends/hw
+# Build directory: /home/upmem0037/xialinl/research/libaray/src/backends/build-dir/hw
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
